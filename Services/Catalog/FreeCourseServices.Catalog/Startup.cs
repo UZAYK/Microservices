37e2607d478cbd04ed03e6f112e4a190629e9f6d
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-06-11 21:44:41 +0300

    implementaions

--- Services/Catalog/FreeCourseServices.Catalog/Startup.cs
+++ Services/Catalog/FreeCourseServices.Catalog/Startup.cs
@@ -1,3 +1,4 @@
+using FreeCourseServices.Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -25,8 +27,20 @@ namespace FreeCourseServices.Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-        services.AddAutoMapper(typeof(Startup));
+            // configuration transactionlarýný bir class üzerinden okuma optionsPattern denir.
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
+
+            services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
+
+            /// IOptions üzerinden DatabaseSettings ulaþmamak için 
+            services.AddSingleton<IDatabaseSettings>(sp =>
+            {
+                // GetRequiredService GetServiceden farklý ilgili service bulamazsa hata fýrlatýr. 
+                /// Mutlaka olmasý gerekir.
+                return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourseServices.Catalog", Version = "v1" });
