a9900d847f169fccf32b7c00b97c8e8b7fbc4004
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-06-13 21:11:32 +0300

    Access specifiers are converted to internal type.

--- Services/Catalog/FreeCourseServices.Catalog/Dtos/CourseDto.cs
+++ Services/Catalog/FreeCourseServices.Catalog/Dtos/CourseDto.cs
@@ -2,7 +2,7 @@
 
 namespace FreeCourseServices.Catalog.Dtos
 {
-    public class CourseDto
+    internal class CourseDto
     {
         public string Id { get; set; }
         public string Name { get; set; }
