dc6f8373f2fc83836d8815d485b9df4af22b729f
Uzay KAHRAMAN <uzaykahramann@gmail.com>
2022-06-14 10:49:09 +0300

    Course Service added.

---
+++ Services/Catalog/FreeCourseServices.Catalog/Services/CourseService.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using FreeCourse.Shared.Dtos;
+using FreeCourseServices.Catalog.Dtos;
+using FreeCourseServices.Catalog.Models;
+using FreeCourseServices.Catalog.Settings;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourseServices.Catalog.Services
+{
+    internal class CourseService
+    {
+        private readonly IMongoCollection<Course> _courseCollection;
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CourseService(IMongoCollection<Course> courseCollection, IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            _mapper = mapper;
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+        }
+        public async Task<Response<List<CourseDto>>> GetAllAsync()
+        {
+            var courses = await _courseCollection.Find(course => true).ToListAsync();
+            if (courses.Any())
+            {
+                foreach (var course in courses)
+                {
+                    course.Category = await _categoryCollection.Find<Category>(x => x.Id == course.CategoryId).FirstAsync();
+                }
+            }
+            else
+            {
+                courses = new List<Course>();
+            }
+            return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
+        }
+
+
+        public async Task<Response<CourseDto>> GetByIdAsync(string id)
+        {
+            var course = await _courseCollection.Find<Course>(x => x.Id == id).FirstOrDefaultAsync();
+            if (course == null)
+            {
+                return Response<CourseDto>.Fail(" Course not found ", 404);
+            }
+            course.Category = await _categoryCollection.Find<Category>(x => x.Id == course.CategoryId).FirstAsync();
+            return Response<CourseDto>.Success(_mapper.Map<CourseDto>(course), 200);
+        }
+        public async Task<Response<List<CourseDto>>> GetAllByUserIdAsync(string userld)
+        {
+            var courses = await _courseCollection.Find<Course>(x => x.UserId == userld).ToListAsync();
+            if (courses.Any())
+            {
+                foreach (var course in courses)
+                {
+                    course.Category = await _categoryCollection.Find<Category>(x => x.Id == course.CategoryId).FirstAsync();
+                }
+            }
+            else { courses = new List<Course>(); }
+            return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
+
+        }
+    }
+}
